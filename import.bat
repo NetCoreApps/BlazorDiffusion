705d5374644d57b592a12d778883c58969e69814
Demis Bellot <demis.bellot@gmail.com>
2022-10-19 16:55:02 +0800

    Update import.bat

--- import.bat
+++ import.bat
@@ -1,3 +1,2 @@
 RD /q /s BlazorDiffusion\App_Files\artifacts
 XCOPY /Y /E /H /C /I ..\BlazorDiffusionAssets\artifacts BlazorDiffusion\App_Files\artifacts
-XCOPY /Y /E /H /C /I ..\BlazorDiffusionAssets\artifacts BlazorDiffusion.Tests\App_Files\artifacts
