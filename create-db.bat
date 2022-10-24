add3e1a3595ed4c06d5c0e27029e26fc14e954e0
Darren Reid <darren@reidmail.org>
2022-10-24 15:53:07 +1100

    Update create db

--- create-db.bat
+++ create-db.bat
@@ -2,7 +2,7 @@ RD /q /s BlazorDiffusion\App_Files\artifacts
 if not exist BlazorDiffusion\App_Files\artifacts md BlazorDiffusion\App_Files\artifacts
 
 IF EXIST BlazorDiffusion\App_Data\db.sqlite DEL BlazorDiffusion\App_Data\db.sqlite
-aws s3 sync s3://diffusion/artifacts BlazorDiffusion\App_Files\artifacts --profile r2 --endpoint-url https://b95f38ca3a6ac31ea582cd624e6eb385.r2.cloudflarestorage.com
+aws s3 sync s3://diffusion/artifacts BlazorDiffusion\App_Files\artifacts --profile r2 --endpoint-url https://b95f38ca3a6ac31ea582cd624e6eb385.r2.cloudflarestorage.com --size-only
 PUSHD BlazorDiffusion
 dotnet run BlazorDiffusion.csproj --AppTasks=migrate
 POPD
