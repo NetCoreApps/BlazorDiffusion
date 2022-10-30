3811c5296fbd36bafc4ed6808595e7e039ab2a3f
Demis Bellot <demis.bellot@gmail.com>
2022-10-30 13:57:20 +0800

    update data

---
+++ BlazorDiffusion.Tests/App_Files/sync.bat
@@ -0,0 +1,6 @@
+RD /q /s artifacts
+MD artifacts\2022\10\20
+
+XCOPY /Y /E /H /C /I ..\..\BlazorDiffusion\App_Files\artifacts\2022\10\20\42753907 artifacts\2022\10\20\42753907
+XCOPY /Y /E /H /C /I ..\..\BlazorDiffusion\App_Files\artifacts\2022\10\20\47519396 artifacts\2022\10\20\47519396
+XCOPY /Y /E /H /C /I ..\..\BlazorDiffusion\App_Files\artifacts\2022\10\20\54857882 artifacts\2022\10\20\54857882
