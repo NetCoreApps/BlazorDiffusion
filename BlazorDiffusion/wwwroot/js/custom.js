2262ea535740ebfa6a5baf3fb7559be8c031daba
Demis Bellot <demis.bellot@gmail.com>
2022-12-20 02:37:05 +0800

    update

--- BlazorDiffusion/wwwroot/js/custom.js
+++ BlazorDiffusion/wwwroot/js/custom.js
@@ -5,12 +5,3 @@ window.scrollTo = (x, y) => {
         return
     return origScrollTo.apply(this, arguments)
 }
-
-JS.getBreakpoints = function () {
-    let resolutions = { '2xl': 1536, xl: 1280, lg: 1024, md: 768, sm: 640 }
-    let sizes = Object.keys(resolutions)
-    let w = document.body.clientWidth
-    let o = {}
-    sizes.forEach(res => o[res] = w > resolutions[res])
-    return o
-}
