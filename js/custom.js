b3229743c6c968dd899094004db0247aa23d15e7
GitHub Action <action@github.com>
2022-12-19 17:57:31 +0000

    deploy: 370c008e4075294012e7cfde20e901bdaa0de668

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+﻿const origScrollTo = window.scrollTo;
+window.scrollTo = (x, y) => {
+    const shouldSkip = true
+    if (x === 0 && y === 0 && shouldSkip)
+        return
+    return origScrollTo.apply(this, arguments)
+}
+
+JS.getBreakpoints = function () {
+    let resolutions = { '2xl': 1536, xl: 1280, lg: 1024, md: 768, sm: 640 }
+    let sizes = Object.keys(resolutions)
+    let w = document.body.clientWidth
+    let o = {}
+    sizes.forEach(res => o[res] = w > resolutions[res])
+    return o
+}
